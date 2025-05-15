c13ef2e3b4a2b92c261b9abdcca59bee16ee12ac
Kevin Espinoza <62354142+ScorpionDYami@users.noreply.github.com>
2025-05-15 13:50:52 -0700

    Crear las clases (vacías) del paquete de entidades y las del paquete de persistencia.

--- src/main/java/mx/itson/atm/entities/Cliente.java
+++ src/main/java/mx/itson/atm/entities/Cliente.java
@@ -1,7 +1,4 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package mx.itson.atm.entities;
 
 /**
@@ -9,5 +6,7 @@ package mx.itson.atm.entities;
  * @author Kevin
  */
 public class Cliente {
+    
+    
     
 }
