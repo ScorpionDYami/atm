8239104f9b0d8e28930a7b27e72f140bbc4cbc12
Jacarial <jan.rios253031@potros.itson.edu.mx>
2025-05-15 22:17:16 -0700

    Se agregaron los metodos para la persistencia de datos de la clase cliente y cuenta

--- src/main/java/mx/itson/atm/persistence/ClienteDAO.java
+++ src/main/java/mx/itson/atm/persistence/ClienteDAO.java
@@ -1,10 +1,22 @@
 
 package mx.itson.atm.persistence;
 
-/**
- *
- * @author Kevin
- */
+import mx.itson.atm.entities.Cliente;
+import org.hibernate.Session;
+
+import java.util.List;
+
 public class ClienteDAO {
-    
+
+    public void guardar(Session session, Cliente cliente) {
+        session.save(cliente);
+    }
+
+    public Cliente obtenerPorId(Session session, int id) {
+        return session.get(Cliente.class, id);
+    }
+
+    public List<Cliente> listarTodos(Session session) {
+        return session.createQuery("FROM Cliente", Cliente.class).list();
+    }
 }
