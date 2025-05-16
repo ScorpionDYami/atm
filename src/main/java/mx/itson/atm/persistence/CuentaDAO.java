3e65a7d382f37922e438babce9c80e69ed08fa80
Jacarial <jan.rios253031@potros.itson.edu.mx>
2025-05-15 22:26:38 -0700

    Se crearon los metodos para cuentaDAO

--- src/main/java/mx/itson/atm/persistence/CuentaDAO.java
+++ src/main/java/mx/itson/atm/persistence/CuentaDAO.java
@@ -1,10 +1,27 @@
 
 package mx.itson.atm.persistence;
 
-/**
- *
- * @author Kevin
- */
+import mx.itson.atm.entities.Cuenta;
+import org.hibernate.Session;
+
+import java.util.List;
+
 public class CuentaDAO {
-    
+
+    public void guardar(Session session, Cuenta cuenta) {
+        session.save(cuenta);
+    }
+
+    public void actualizar(Session session, Cuenta cuenta) {
+        session.update(cuenta);
+    }
+
+    public Cuenta obtenerPorId(Session session, int id) {
+        return session.get(Cuenta.class, id);
+    }
+
+    public List<Cuenta> listarTodas(Session session) {
+        return session.createQuery("FROM Cuenta", Cuenta.class).list();
+    }
 }
+
